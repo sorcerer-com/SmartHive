8f715e361ed0a6bd03b8d7748054fe6c0bf328b5
Hristo Kapanakov <sorcerer_com@abv.bg>
2018-03-29 22:13:40 +0300

    Add simple index page and AddData API.

--- Service.py
+++ Service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import *
+from datetime import datetime, timedelta
 
 from libs.Logger import *
 from libs.Connection import *
@@ -12,16 +13,54 @@ app = Flask(__name__, template_folder = "./")
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	with Connection() as conn:
+		dt = datetime.now() - timedelta(days=10)
+		values = conn.execute(
+			"SELECT SensorId, DateTime, Type, Value FROM data "
+			"WHERE datetime(DateTime) > datetime('%s')" % dt).fetchall()
+	
+	data = {}
+	for value in values:
+		if value[0] not in data:
+			data[value[0]] = {}
+		if value[1] not in data[value[0]]:
+			data[value[0]][value[1]] = {}
+		if value[2] not in data[value[0]][value[1]]:
+			data[value[0]][value[1]][value[2]] = {}
+		data[value[0]][value[1]][value[2]] = value[3]
+	
+	return render_template("index.html", data=data)
 	
 @app.route("/lastSensorId")
 def lastSensorId():
 	id = 0
 	with Connection() as conn:
-		row = conn.read("SELECT MAX(SensorId) FROM data").fetchone()
+		row = conn.execute("SELECT MAX(SensorId) FROM data").fetchone()
 		if row != None and row[0] != None:
 			id = row[0]
-	return str(id)	
+	return str(id)
+
+@app.route("/AddData/<sensorId>", methods=["GET", "POST"])
+def AddData(sensorId):
+	data = request.form if request.method == "POST" else request.args
+	if ("type" in data) and ("value" in data):
+		dt = datetime.now().replace(second=0, microsecond=0)
+		if "deltatime" in data:
+			dt -= timedelta(minutes=float(data["deltatime"]))
+			
+		with Connection() as conn:
+			prevValue = conn.execute(
+				"SELECT Value FROM data " 
+				"WHERE SensorId = %s AND Type LIKE '%s' AND datetime(DateTime) < datetime('%s') "
+				"ORDER BY datetime(DateTime) DESC LIMIT(1)" \
+				% (int(sensorId), data["type"], dt)).fetchone()
+				
+			if prevValue != None and float(prevValue[0]) != float(data["value"]):
+				conn.execute("INSERT INTO data(SensorId, DateTime, Type, Value) VALUES(?, ?, ?, ?)", \
+					int(sensorId), dt, str(data["type"]), float(data["value"]))
+				conn.commit()
+
+	return redirect("/")
 	
 @app.route("/restart")
 def restart():
--- libs/Connection.py
+++ libs/Connection.py
@@ -1,19 +1,38 @@
 import sqlite3
 
+from Logger import *
+
 class Connection:
 	DatabaseFile = None
+	Debug = False
 	
 	def __init__(self):
 		self.conn = None
+		self.curs = None
 		
 	def __enter__(self):
 		if Connection.DatabaseFile != None:
-			self.conn = sqlite3.connect(Connection.DatabaseFile)
+			Connection.debug("Connect to: " + Connection.DatabaseFile)
+			self.conn = sqlite3.connect(Connection.DatabaseFile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+			self.curs = self.conn.cursor()
 		return self
 	
 	def __exit__(self, exc_type, exc_value, traceback):
+		Connection.debug("Close connection")
 		self.conn.close()
 		
-	def read(self, sql):
-		curs = self.conn.cursor()
-		return curs.execute(sql)
+	def execute(self, sql, *args):
+		Connection.debug("Execute: " + sql + str(args))
+		return self.curs.execute(sql, args)
+		
+	def commit(self):
+		Connection.debug("Commit")
+		self.conn.commit()
+	
+	
+	@staticmethod
+	def debug(text):
+		if Connection.Debug:
+			Logger.log("info", "Connection - " + text)
+		else:
+			Logger.log("debug", "Connection - " + text)
